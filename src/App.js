a4420300fd345b767e1015887a72638478d0e73b
Adrianasndrs <112991445+Adrianasndrs@users.noreply.github.com>
2023-03-19 11:59:51 -0400

    test deplot commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React! 
+          Learn React at MSU! 
         </a>
       </header>
     </div>
