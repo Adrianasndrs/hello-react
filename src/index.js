f13319830354acc73733ffc7b40b03526cbb0cdb
Adrianasndrs <112991445+Adrianasndrs@users.noreply.github.com>
2023-03-26 15:27:01 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
