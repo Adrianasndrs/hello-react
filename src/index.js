25ebc71244e3a8fc74d39ed369497db6956a21d3
Adrianasndrs <112991445+Adrianasndrs@users.noreply.github.com>
2023-03-26 15:26:05 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -13,26 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-const CarouselPage = () => {
-    return ( 
-      <Carousel>
-        <div>
-          <img src="https://placehold.co/100x100" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src="https://placehold.co/200x200" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src="https://placehold.co/300x300" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
-    );
-  };
-  
-  export default CarouselPage;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
