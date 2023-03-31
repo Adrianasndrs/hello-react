fe5b13089a7c161715fd822c6e9a3550745e741e
Adrianasndrs <112991445+Adrianasndrs@users.noreply.github.com>
2023-03-31 13:18:01 -0400

    React + Supabase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-
+import { useState } from 'react';
+import { supabase } from './supabaseClient';
 import './App.css';
+
+// A React component that queries and displays data from Supabase
+function Library() {
+  // The useState hook lets us store data in a component across renders
+  // setMyBooks is a setter function that updates the state of myBooks
+  const [myBooks, setMyBooks] = useState([]);
+  // This should look familar from Codepen
+  async function getBooks() {
+    let { data: books, error } = await supabase
+      .from('books')
+      .select('*')
+    // Update the state
+    setMyBooks(books);
+  }
+  // Execute the function
+  getBooks();
+  // Below is what displays when you use <Library />
+  return (
+    <table>
+    {
+      myBooks.map(b => (
+        <tr>
+          <td>{b.title}</td>
+          <td>{b.author}</td>
+          <td>{b.isbn}</td>
+        </tr>
+      ))
+    }
+    </table>
+  )
+}
 
 const magazines = [
   { id: 1, title: 'Second Hand News', isAvailable: true },
@@ -67,26 +99,31 @@ function Musicshelf() {
 
   
 function MagicButton() {
+  const[count, setCount] = useState(0);
+  function doMagic(){
+    setCount(count + 1);
+  }
   return(
     <>
     <h3>This is a magic button</h3>
 
-    <button onClick={sing}>Magic</button>
+    <button onClick={doMagic}>Magic {count}</button>
+    <button onClick={sing}>Sing</button>
     </>
   )
 }
 
 function sing() {
   alert('You can go your own way!');
+  
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Musicshelf />
-        <SongList />
-        <MagicButton />
+        <Library />
+
       </header>
     </div>
   );
